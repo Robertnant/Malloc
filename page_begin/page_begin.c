5ebef64918527f0f800882da639aa233c5922f92
Robert Nantchouang Nantchouang <robert.nantchouang-nantchouang@epita.fr>
2022-02-08 14:32:17 +0100

    page_begin: implement

---
+++ page_begin/page_begin.c
@@ -0,0 +1,9 @@
+#include "page_begin.h"
+
+void *page_begin(void *ptr, size_t page_size)
+{
+    char *char_ptr = ptr;
+
+    size_t start = (size_t) ptr & (page_size - 1);
+    return char_ptr - start;
+}
