5fc128f2b4722963e61165ebf12d123ef7af9cab
Robert Nantchouang Nantchouang <robert.nantchouang-nantchouang@epita.fr>
2022-02-27 13:32:31 +0100

    block_allocator: use mmap and munamp

--- block_allocator/allocator.c
+++ block_allocator/allocator.c
@@ -1,4 +1,10 @@
+#define _GNU_SOURCE
+
 #include "allocator.h"
+
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 // Allocates and returns a new blk_allocator structure.
 struct blk_allocator *blka_new(void)
@@ -9,9 +15,22 @@ struct blk_allocator *blka_new(void)
 // Allocates a blk_meta structure and adds to head of a blk_allocator.
 struct blk_meta *blka_alloc(struct blk_allocator *blka, size_t size)
 {
-    struct blk_meta *new = malloc(sizeof(struct blk_meta));
-    new->size = size;
-    new->data = malloc(size * sizeof(char));
+    // Get the system's page size.
+    long pagesize = sysconf(_SC_PAGESIZE);
+
+    /*
+    ** Calculate number of pages of length pagesize needed to hold at least size
+    ** amount of data.
+    */
+
+    // Sum of requested size and size of fields in blk_meta.
+    size_t total_size = size + sizeof(struct blk_meta);
+    size_t nb_pages = (total_size / pagesize) + (total_size % pagesize);
+
+    // Length allocated is rounded up automatically to multiple of pagesize.
+    struct blk_meta *new = mmap(NULL, total_size, PROT_READ | PROT_WRITE,
+                                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    new->size = nb_pages *pagesize - sizeof(struct blk_meta);
     new->next = blka->meta;
 
     return new;
@@ -20,16 +39,15 @@ struct blk_meta *blka_alloc(struct blk_allocator *blka, size_t size)
 // Deallocates memory of a given blk_meta structure.
 void blka_free(struct blk_meta *blk)
 {
-    if (blk->size)
-        free(blk->data);
-
-    free(blk);
+    munmap(blk, blk->size + sizeof(struct blk_meta));
 }
 
 // De-allocates memory of the first blk_meta structure in a block allocator.
 void blka_pop(struct blk_allocator *blka)
 {
+    struct blk_meta *next = blka->meta->next;
     blka_free(blka->meta);
+    blka->meta = next;
 }
 
 // Free allocated blk_allocator.
@@ -37,8 +55,6 @@ void blka_delete(struct blk_allocator *blka)
 {
     while (blka->meta)
     {
-        struct blk_meta *next = blka->meta->next;
-        blka_pop(blka->meta);
-        blka->meta = next;
+        blka_pop(blka);
     }
 }
--- page_begin/page_begin.c
+++ page_begin/page_begin.c
@@ -4,6 +4,6 @@ void *page_begin(void *ptr, size_t page_size)
 {
     char *char_ptr = ptr;
 
-    size_t start = (size_t) ptr & (page_size - 1);
+    size_t start = (size_t)ptr & (page_size - 1);
     return char_ptr - start;
 }
