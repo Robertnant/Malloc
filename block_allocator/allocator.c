037aa623a467ed72dc214e3a33f89c1c0d880fe9
Robert Nantchouang Nantchouang <robert.nantchouang-nantchouang@epita.fr>
2022-02-27 13:38:23 +0100

    block_allocator: excluded size of structure fields in mmap call

--- block_allocator/allocator.c
+++ block_allocator/allocator.c
@@ -22,15 +22,12 @@ struct blk_meta *blka_alloc(struct blk_allocator *blka, size_t size)
     ** Calculate number of pages of length pagesize needed to hold at least size
     ** amount of data.
     */
-
-    // Sum of requested size and size of fields in blk_meta.
-    size_t total_size = size + sizeof(struct blk_meta);
-    size_t nb_pages = (total_size / pagesize) + (total_size % pagesize);
+    size_t nb_pages = (size / pagesize) + (size % pagesize);
 
     // Length allocated is rounded up automatically to multiple of pagesize.
-    struct blk_meta *new = mmap(NULL, total_size, PROT_READ | PROT_WRITE,
-                                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    new->size = nb_pages *pagesize - sizeof(struct blk_meta);
+    struct blk_meta *new = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE|
+            MAP_ANONYMOUS, -1, 0);
+    new->size = nb_pages * pagesize;
     new->next = blka->meta;
 
     return new;
@@ -39,7 +36,7 @@ struct blk_meta *blka_alloc(struct blk_allocator *blka, size_t size)
 // Deallocates memory of a given blk_meta structure.
 void blka_free(struct blk_meta *blk)
 {
-    munmap(blk, blk->size + sizeof(struct blk_meta));
+    munmap(blk, blk->size);
 }
 
 // De-allocates memory of the first blk_meta structure in a block allocator.
