a9ee761fc26610cba6fc7dba723df7f9675d1955
Robert Nantchouang Nantchouang <robert.nantchouang-nantchouang@epita.fr>
2022-03-13 01:37:22 +0100

    malloc: convert free list to structure

--- src/tools.h
+++ src/tools.h
@@ -4,16 +4,18 @@
 #include <stddef.h>
 #include <unistd.h>
 
+#include "malloc.h"
+
 #define FULL(A) ((A) == 0)
 #define SIZE_BITS sizeof(size_t) * 8
 #define SET_FREE(NUM, POS) ((NUM) |= 1ULL << (POS))
 #define SET_USED(NUM, POS) ((NUM) &= ~(1ULL << (POS)))
 #define IS_SET(NUM, POS) ((NUM) & (1ULL << (POS)))
 
-int mark_block(size_t *free_list, size_t size);
+int mark_block(struct free_list *free_list, size_t size);
 void *get_block(void *bucket, int n, size_t block_size);
 void *page_begin(void *ptr, size_t page_size);
-void set_free(size_t pos, size_t *free_list);
-void reset_list(size_t *free_list, size_t block_size);
+void set_free(size_t pos, struct free_list *free_list);
+void reset_list(struct free_list *free_list, size_t block_size);
 
 #endif /* !TOOLS_H */
